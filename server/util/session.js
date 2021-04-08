ca8ba7923b39b4e09b0efd48d41fc8daaa2e743d
teodor <31542280+tgbv@users.noreply.github.com>
2021-04-08 22:56:09 +0300

    8th commit
    - Replaced express-mysql-session with connect-session-sequelize
    - Updated blacklist

--- server/util/session.js
+++ server/util/session.js
@@ -2,21 +2,38 @@
 *   handles sessions
     stored in an util because it allows me to use it in what route I like
 */
+// express-mysql-session
 const Session = require('express-session')
-const MySQLStore = require('express-mysql-session')(Session);
+const SQLStore = require('connect-session-sequelize')(Session.Store);
+const {DataTypes} = require('sequelize')
+const { DB } = require('../server')
 
+// sessions model
+DB.define("sessions", {
+    expires: DataTypes.DATE,
+    data: DataTypes.TEXT,
+    sid: {
+        type: DataTypes.STRING,
+        primaryKey: true,
+    }
+}, {
+    initialAutoIncrement: false,
+});
+
+// sequelize instance
 const Instance = Session({
     secret: process.env.APP_KEY,
     resave: false,
     saveUninitialized: true,
     cookie: { secure: 'auto' },
-    store: new MySQLStore({
-        host: process.env.DB_HOST,
-        port: process.env.DB_PORT,
-        user: process.env.DB_USR,
-        password: process.env.DB_PSS,
-        database: process.env.DB_NAME,
-    })
+    store: new SQLStore({
+        db: DB,
+        table: 'sessions',
+        checkExpirationInterval: 15 * 60 * 1000, // The interval at which to cleanup expired sessions in milliseconds.
+        expiration: 60 * 60 * 1000,  // The maximum age (in milliseconds) of a valid session.
+    }),
+    resave: false,
+    proxy: false,
 })
 
 module.exports = Instance
