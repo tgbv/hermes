ed8c41dc4bbfdc2d7de621317786c32aa6248809
teodor <31542280+tgbv@users.noreply.github.com>
2021-06-02 12:59:18 +0300

    bugfix

--- server/route/pub.js
+++ server/route/pub.js
@@ -4,7 +4,7 @@ module.exports = require('express').Router()
     /*
     *   for public files
     */
-    .get('/:target*', (req, res, next)=>{
+    .get('/:target(*)', (req, res, next)=>{
        
         let p = Path.resolve(`${__dirname}/../pub/${req.params.target}`)
         res.setHeader("Cache-Control", `max-age=${60*15}, must-revalidate`)
