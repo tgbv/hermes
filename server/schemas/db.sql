98463a8bc5ef463038bb4cad410ecd2e771e6fe4
teodor <31542280+tgbv@users.noreply.github.com>
2021-06-05 20:50:49 +0300

    Updated SQL schema

--- server/schemas/db.sql
+++ server/schemas/db.sql
@@ -2,17 +2,46 @@ SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
 SET time_zone = "+00:00";
 
+
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
 /*!40101 SET NAMES utf8mb4 */;
 
+--
+-- Database: `hermes`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `api_throttles`
+--
 
 CREATE TABLE `api_throttles` (
   `user_id` mediumint(8) UNSIGNED NOT NULL,
   `count` smallint(5) UNSIGNED NOT NULL DEFAULT 0,
   `touched` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `pr_tokens`
+--
+
+CREATE TABLE `pr_tokens` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `token` varchar(512) NOT NULL,
+  `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
+  `updatedAt` datetime DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sent_messages`
+--
 
 CREATE TABLE `sent_messages` (
   `id` int(10) UNSIGNED NOT NULL,
@@ -24,6 +53,12 @@ CREATE TABLE `sent_messages` (
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `sessions`
+--
+
 CREATE TABLE `sessions` (
   `sid` varchar(128) CHARACTER SET ascii COLLATE ascii_general_nopad_ci NOT NULL,
   `expires` datetime DEFAULT NULL,
@@ -31,6 +66,12 @@ CREATE TABLE `sessions` (
   `createdAt` datetime NOT NULL DEFAULT current_timestamp(),
   `updatedAt` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tickets`
+--
 
 CREATE TABLE `tickets` (
   `id` mediumint(8) UNSIGNED NOT NULL,
@@ -42,6 +83,12 @@ CREATE TABLE `tickets` (
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `ticket_chat`
+--
+
 CREATE TABLE `ticket_chat` (
   `id` int(10) UNSIGNED NOT NULL,
   `ticket_id` mediumint(8) UNSIGNED NOT NULL,
@@ -49,6 +96,24 @@ CREATE TABLE `ticket_chat` (
   `message` text NOT NULL,
   `created_at` datetime NOT NULL DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `tofa_tokens`
+--
+
+CREATE TABLE `tofa_tokens` (
+  `user_id` int(10) UNSIGNED NOT NULL,
+  `uri` varchar(512) NOT NULL,
+  `auth_token` varchar(32) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `users`
+--
 
 CREATE TABLE `users` (
   `id` mediumint(8) UNSIGNED NOT NULL,
@@ -61,41 +126,89 @@ CREATE TABLE `users` (
   `updated_at` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
+--
+-- Indexes for dumped tables
+--
 
+--
+-- Indexes for table `api_throttles`
+--
 ALTER TABLE `api_throttles`
   ADD PRIMARY KEY (`user_id`);
 
+--
+-- Indexes for table `pr_tokens`
+--
+ALTER TABLE `pr_tokens`
+  ADD KEY `user_id` (`user_id`);
+
+--
+-- Indexes for table `sent_messages`
+--
 ALTER TABLE `sent_messages`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`,`receipt`);
 
+--
+-- Indexes for table `sessions`
+--
 ALTER TABLE `sessions`
   ADD PRIMARY KEY (`sid`);
 
+--
+-- Indexes for table `tickets`
+--
 ALTER TABLE `tickets`
   ADD PRIMARY KEY (`id`),
   ADD KEY `user_id` (`user_id`);
 ALTER TABLE `tickets` ADD FULLTEXT KEY `topic` (`topic`);
 
+--
+-- Indexes for table `ticket_chat`
+--
 ALTER TABLE `ticket_chat`
   ADD PRIMARY KEY (`id`),
   ADD KEY `ticket_id` (`ticket_id`,`created_by`);
 
+--
+-- Indexes for table `tofa_tokens`
+--
+ALTER TABLE `tofa_tokens`
+  ADD PRIMARY KEY (`user_id`);
+
+--
+-- Indexes for table `users`
+--
 ALTER TABLE `users`
   ADD PRIMARY KEY (`id`),
   ADD UNIQUE KEY `username` (`username`),
   ADD KEY `apikey` (`apikeysalt`) USING BTREE;
 
+--
+-- AUTO_INCREMENT for dumped tables
+--
 
+--
+-- AUTO_INCREMENT for table `sent_messages`
+--
 ALTER TABLE `sent_messages`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `tickets`
+--
 ALTER TABLE `tickets`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `ticket_chat`
+--
 ALTER TABLE `ticket_chat`
   MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT;
 
+--
+-- AUTO_INCREMENT for table `users`
+--
 ALTER TABLE `users`
   MODIFY `id` mediumint(8) UNSIGNED NOT NULL AUTO_INCREMENT;
 COMMIT;
